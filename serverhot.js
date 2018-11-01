ecf808b54f270467cd6e53535e6de335e15ac1fd
Kysper <kysper50@gmail.com>
2018-10-31 20:26:23 -0500

    Added object port

--- serverhot.js
+++ serverhot.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 
 app.use(express.urlencoded({extended: true}));
